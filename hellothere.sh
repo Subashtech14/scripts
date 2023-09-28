4154a8fb5f2f0e431fba38bb2ed0d05b83d77115
root <root@Ubuntu.myguest.virtualbox.org>
2023-09-28 09:23:58 +0530

    positional arguments and if else if etc..

---
+++ hellothere.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FIRST_NAME=Hebert
+LAST_NAME=Lindemans
+
+echo Hello $FIRST_NAME $LAST_NAME
+ 
+echo $ls/usr/
---
+++ interactiveshell.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "What is your First Name ?"
+read FIRST_NAME
+echo "What is your Last Name ?"
+read  LAST_NAME
+
+echo Hello $FIRST_NAME $LAST_NAME
